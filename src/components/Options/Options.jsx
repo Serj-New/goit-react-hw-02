db43f7fb63c6bebf404ca4a31c7d2f300e684d4b
Sergey Novikov <69253755+Serj-New@users.noreply.github.com>
2024-04-14 00:32:16 +0300

    magic style

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,7 +2,7 @@ import css from "./Options.module.css";
 
 export default function Options({ updateFeedback, totalFeedback, resetFeedback }) {
     return (
-        <div>
+        <div className={css.btn}>
             <button onClick={() => updateFeedback('good')}>Good</button>
             <button onClick={() => updateFeedback('neutral')}>Neutral</button>
             <button onClick={() => updateFeedback('bad')}>Bad</button>
